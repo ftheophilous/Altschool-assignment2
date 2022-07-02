4ae345710a5074422d1ecb7dd62e65cc2189e1fd
Theophilous Francis <ftheophilous@gmail.com>
2022-07-02 21:32:05 +0000

    Challenge 1: I changed the code so that it returns the sum of all the numbers in the array
    Challenge 3: I changed the code so that it returns the corresponding results in fahrenheit in a new array

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,15 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0;
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+      sum = sum + arrayOfNumbers[i];
+    }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+assignment.sumOfNumbers = sumOfNumbers;
+
 
 /**
  * Challenge - 2
@@ -47,9 +53,15 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+    let newArray = [];
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+     
+      let fahrenheitValue = Math.trunc((arrayOfNumbers[i] * (9/5)) + 32);
+      newArray.push(fahrenheitValue);
+    }
+    return newArray;
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
